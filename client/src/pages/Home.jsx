a8204c150611676055d547185462d1ccf0576ae0
EzioPearce <krishna.vaddamani@gmail.com>
2023-03-22 21:22:59 +0530

    give up on the big laptop

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,8 +2,17 @@ import React, {useState, useEffect} from 'react';
 import { Loader, FormField, Card } from '../components';
 
 const Home = () => {
+  const [Loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
   return (
-    <div>Home</div>
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="absolute left-5 font-extrabold text-[#222328] text-[32px]">Community Showcase</h1>
+      </div>
+      <div>
+        <p className="mt-10 text-[#666e75] text-[16px] max-w [500px]">Browse through a collection of imaginary pictures...Because you're a Shit artist</p>
+      </div>
+    </section>
   )
 }
 
